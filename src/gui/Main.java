af41ab7e7fc5f99c813ebf6384d177f5c725fee1
Manuel Maria Madeira Silvestre <mmmse@iscte-iul.pt>
2023-12-09 21:37:32 +0000

    Mais alteracoes

--- src/gui/Main.java
+++ src/gui/Main.java
@@ -11,26 +11,6 @@ import game.Server;
 public class Main {
 	public static void main(String[] args) {
 		LocalBoard board = new LocalBoard();
-        
-        // Declare the server variable as final
-        final Server[] server = {null};
-        
-        try {
-            server[0] = new Server();
-            
-            // Start the server in a separate thread
-            Thread serverThread = new Thread(() -> {
-                try {
-                    server[0].startServer();
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            });
-            
-            serverThread.start();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
 
         // Initialize and start the GUI
         SnakeGui game = new SnakeGui(board, 600, 0);
